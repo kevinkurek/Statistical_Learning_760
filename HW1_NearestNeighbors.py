1875bc4573120ea7b717dc1b970fdbfb06f1b5e8
Kevin Kurek <kevinkurek@live.com>
2018-01-30 13:44:36 -0800

    - Added Prediction Method to clean up code when fitting on X_train,y_train and predicting on X_test

--- HW1_NearestNeighbors.py
+++ HW1_NearestNeighbors.py
@@ -31,8 +31,9 @@ df_9['Digit'] = 9
 
 class knn_algo():
 
-  def __init__(self,df_list):
+  def __init__(self,df_list,neighbors_for_prediction):
     self.df_list = df_list
+    self.neighbors = neighbors_for_prediction
 
   # Concatentate digit 0 and digit 3 dataframes together
   def concat_dfs(self):
@@ -44,7 +45,7 @@ class knn_algo():
   def features(self):
     self.X = self.df.iloc[:,:-1]
     self.y = self.df.iloc[:,-1]
-    print(len(X.columns)) # Make sure there are 256 input features
+    #print(len(X.columns)) # Make sure there are 256 input features
     return self.X,self.y
 
   def knn_model(self):
@@ -65,21 +66,30 @@ class knn_algo():
     index, value = max(enumerate(cv_scores), key=operator.itemgetter(1))
     print('The maximum accuracy is: {}'.format(value))
     print('The optimal number of neighbors is: {}'.format(index+1))
-    # print(confusion_matrix(self.y_test, self.y_pred))
-    # print(classification_report(self.y_test,self.y_pred))
     return 'It worked'
 
-first = knn_algo([df_0,df_3])
+  def prediction(self):
+    self.knn = KNeighborsClassifier(n_neighbors=self.neighbors)
+    self.knn.fit(self.X_train,self.y_train)
+    self.y_pred = self.knn.predict(self.X_test)
+    print(confusion_matrix(self.y_test, self.y_pred))
+    #print(classification_report(self.y_test,self.y_pred))
+    return classification_report(self.y_test,self.y_pred)
+
+first = knn_algo([df_0,df_3],1)
 first.concat_dfs()
 first.features()
 print(first.knn_model())
+print(first.prediction())
 
-second = knn_algo([df_2,df_7])
+second = knn_algo([df_2,df_7],2)
 second.concat_dfs()
 second.features()
 print(second.knn_model())
+print(second.prediction())
 
-third = knn_algo([df_1,df_9])
+third = knn_algo([df_1,df_9],2)
 third.concat_dfs()
 third.features()
 print(third.knn_model())
+print(third.prediction())
